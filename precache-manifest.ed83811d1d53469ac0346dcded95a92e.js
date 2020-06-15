1661947538af3db65a8a6ddd51293f260d0e0540
Joshua Carew <joshua.carew@yahoo.com>
2020-06-15 12:50:26 +0100

    Updates

---
+++ precache-manifest.ed83811d1d53469ac0346dcded95a92e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc97be67ed136b2584838a804717920e",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "a2b6a9a42c16150af1f3",
+    "url": "/portfolio-app/static/css/main.c446d755.chunk.css"
+  },
+  {
+    "revision": "08d2fe7e9904f66beb55",
+    "url": "/portfolio-app/static/js/2.23c639c3.chunk.js"
+  },
+  {
+    "revision": "500c79c5aa03ea92413150b73c3e7096",
+    "url": "/portfolio-app/static/js/2.23c639c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b6a9a42c16150af1f3",
+    "url": "/portfolio-app/static/js/main.16dff390.chunk.js"
+  },
+  {
+    "revision": "be4865196d4626f9c86b",
+    "url": "/portfolio-app/static/js/runtime-main.147ed822.js"
+  },
+  {
+    "revision": "285a803ec5682cb8bbb39b419e854e7c",
+    "url": "/portfolio-app/static/media/calculator.3872404b.png"
+  },
+  {
+    "revision": "e3f21cbd65ac155a0eab56c0bab375b4",
+    "url": "/portfolio-app/static/media/down-arrow.e3f21cbd.svg"
+  },
+  {
+    "revision": "3785c1cb9f9c48207a1bc2cb5260059e",
+    "url": "/portfolio-app/static/media/etchasketch.970893a5.png"
+  },
+  {
+    "revision": "81b8ad49f84029b97e08ff4ace55cfd3",
+    "url": "/portfolio-app/static/media/googlehomepage.6639411b.png"
+  },
+  {
+    "revision": "262ce13a32d0b69a4353d7c746f79609",
+    "url": "/portfolio-app/static/media/librarypage.54db0408.png"
+  },
+  {
+    "revision": "3721353448f15115aa2ea6aa67b9238e",
+    "url": "/portfolio-app/static/media/newsweek.f1e7ccae.png"
+  },
+  {
+    "revision": "dbe24f6b11769766ffa0d459d786e200",
+    "url": "/portfolio-app/static/media/newyorktimes.f0d1a94e.png"
+  },
+  {
+    "revision": "207743b9b247a20dcdeb6c1488e01a4c",
+    "url": "/portfolio-app/static/media/portfolio.6d58babf.png"
+  },
+  {
+    "revision": "ee24b8c1484e9d849bc46b478eb18ae5",
+    "url": "/portfolio-app/static/media/profileimg.a39d3628.jpg"
+  },
+  {
+    "revision": "7090665c164f97d349dae54ca1603734",
+    "url": "/portfolio-app/static/media/restaurant.b81dbff9.png"
+  },
+  {
+    "revision": "e4d9ca0e7cd2d2f0d0dc044f208f24fe",
+    "url": "/portfolio-app/static/media/rockpaperscissors.6be5e5b2.png"
+  },
+  {
+    "revision": "332fc51df3557047086147115ee27455",
+    "url": "/portfolio-app/static/media/tictactoe.073a8334.png"
+  },
+  {
+    "revision": "9c639548ff551c69b994a86cbb04751e",
+    "url": "/portfolio-app/static/media/tnw.c6fa586c.png"
+  },
+  {
+    "revision": "6b8eb66d8bb15ea54de4d4830f78cc04",
+    "url": "/portfolio-app/static/media/todolist.2588f488.png"
+  },
+  {
+    "revision": "82b6cf32f8ab53e863ec4915598af405",
+    "url": "/portfolio-app/static/media/weather.e2a281a4.png"
+  }
+]);
